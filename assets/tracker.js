e06286def107106356c168c37d6d8f134c3be875
Dane Harrigan <dane.harrigan@gmail.com>
2013-06-11 12:37:02 -0700

    collecting basic user data

--- assets/tracker.js
+++ assets/tracker.js
@@ -1,23 +1,54 @@
-_ts1 = _ts1 || []
+_ts1 = _ts1 || [];
 (function(){
-  _ts1 = {
-    // return a string of param args
-    Params: function() {
+	console.log("[ts1] debug: start");
 
-    },
+	var acc = undefined
 
-    // trigger new tracker
-    Push: function() {
+	function Track() {
+		console.log("[ts1] debug: track")
 
-    }
-  }
+		var img = new Image()
+		img.src = "/tracker.gif?" + Params()
+	}
 
-  var r = screen.width + "x" + screen.height,
-      w = screen.availWidth,
-      h = screen.availHeight,
-      d = screen.pixelDepth || screen.colorDepth,
-      pr = window.devicePixelRatio,
-      a = navigator.userAgent
+	function Params() {
+		var resolution   = screen.width + "x" + screen.height,
+		    screenWidth  = screen.availWidth,
+		    screenHeight = screen.availHeight,
+		    colorRange   = screen.pixelDepth || screen.colorDepth,
+		    pixelRatio   = window.devicePixelRatio,
+		    userAgent    = navigator.userAgent,
+		    pageTitle 	 = document.title,
+		    pathName     = window.location.pathname,
+		    referrer     = document.referrer
 
-  
+		var params = "" +
+			"&r=" + resolution +
+			"&w=" + screenWidth +
+			"&h=" + screenHeight +
+			"&c=" + colorRange +
+			"&p=" + pixelRatio +
+			"&a=" + escape(userAgent) +
+			"&t=" + escape(pageTitle) +
+			"&u=" + escape(pathName) +
+			"rf=" + escape(referrer)
+
+		console.log("[ts1] debug: params " + params)
+		return params
+	}
+
+	function Push(args) {
+		console.log("[ts1] debug: push")
+		switch(args[0]) {
+			case "account": acc = args[1]; break
+			case "track": Track(); break
+		}
+	}
+
+	_ts1.push = Push
+	while(args = _ts1.shift()) {
+		_ts1.push(args)
+	}
+
+	console.log("[ts1] debug: finish");
 })()
